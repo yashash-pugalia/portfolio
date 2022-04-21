2ee0858dbf5bb1a5e63f37fef140bfcb268ded25
Yashash <yashash1511@gmail.com>
2022-04-21 09:00:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ export default function App() {
   return (
     <>
       <nav>
-        <h1>zxcv</h1>
+        <h1>lkj</h1>
         <a href="#about">About</a>
         <a href="#projects">Projects</a>
         <a
