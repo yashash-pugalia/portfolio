f40ce6bb6806474dfbcfc0de3c2407485e96b64c
Yashash <yashash1511@gmail.com>
2022-04-25 09:45:47 +0530

    commit message

---
+++ src/components/Projects/ProjectCard.js
@@ -0,0 +1,25 @@
+import "./Projects.scss";
+import { FaGithub, FaArrowRight } from "react-icons/fa";
+
+export const ProjectCard = ({ img, title, desc, repoLink, website }) => {
+  return (
+    <div className="projectCard cardBox">
+      <h3>{title}</h3>
+      <img src={img} alt={title} width="272" height="153" />
+      <p>{desc}</p>
+      <div className="btnBox">
+        <a href={website} target="_blank" rel="noreferrer" className="btn">
+          <FaArrowRight /> View Live
+        </a>
+        <a
+          href={repoLink}
+          target="_blank"
+          rel="noreferrer"
+          className="btn btn-github"
+        >
+          <FaGithub /> Github
+        </a>
+      </div>
+    </div>
+  );
+};
