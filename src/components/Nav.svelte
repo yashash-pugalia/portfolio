a1cb0f615a78287dcf08f9a6e2837380d2f49f2b
Yashash Pugalia <yashash1511@gmail.com>
2022-08-14 16:20:31 +0000

    Initial Commit

---
+++ src/components/Nav.svelte
@@ -0,0 +1,64 @@
+<nav>
+	<h1>MENU...</h1>
+	<a href="#about">About</a>
+	<a href="#projects">Projects</a>
+	<a href="http://github.com/yashash-pugalia" target="_blank" rel="noreferrer"> Github </a>
+	<a href="mailto:yashash1511@gmail.com" target="_blank" rel="noreferrer"> Contact </a>
+</nav>
+
+<style>
+	nav {
+		display: flex;
+		align-items: center;
+		justify-content: space-around;
+		position: sticky;
+		top: 2rem;
+		padding: 1rem;
+		gap: 1rem;
+		background: black;
+		box-shadow: rgb(255 215 77 / 60%) -1px -1px 6px, rgb(124 127 255 / 60%) 1px 1px 6px;
+		z-index: 10;
+
+		width: calc(126px * 2);
+		max-width: calc(100vw - 1rem);
+		height: calc(37px * 2);
+		margin: 0 auto;
+		border-radius: 4rem;
+
+		transform: scale(0.5);
+		transform-origin: top;
+		transition: 200ms;
+	}
+	nav a {
+		display: none;
+		pointer-events: none;
+	}
+	nav:hover {
+		transform: none;
+		border-radius: 1rem;
+		width: max-content;
+	}
+	nav:hover a {
+		display: initial;
+		animation: a-click-fix 200ms 1 forwards;
+	}
+	@keyframes a-click-fix {
+		100% {
+			pointer-events: all;
+		}
+	}
+	nav:hover h1 {
+		display: none;
+	}
+
+	a {
+		font-family: 'Roboto Mono', monospace;
+		color: inherit;
+		transition: all 200ms;
+		padding: 4px 8px;
+		border-radius: 4px;
+	}
+	a:hover {
+		text-shadow: rgb(255 215 77 / 60%) -1px -1px 6px, rgb(124 127 255 / 60%) 1px 1px 6px;
+	}
+</style>
---
+++ src/components/Projects.svelte
@@ -0,0 +1,113 @@
+<script lang="ts">
+	const getRepos = (async () => {
+		const response = await fetch('https://api.github.com/users/yashash-pugalia/repos?type=all');
+		return await response.json();
+	})();
+</script>
+
+<section id="projects">
+	<h2>Projects</h2>
+	<div class="projectCont">
+		{#await getRepos}
+			<p>Loading...</p>
+		{:then repos}
+			{#each repos as r}
+				{#if !r.fork}
+					<div class="projectCard cardBox" style:--bgImg="url('{r.homepage}/demo.webp')">
+						<h3>
+							{r.name.charAt(0).toUpperCase() + r.name.slice(1).replace(/([A-Z]{1,})/g, ' $1')}
+						</h3>
+						<p>{r.description}</p>
+						<div class="topics">
+							{#each r.topics as topic}
+								<span class:uppercase={topic.length < 5}>{topic}</span>
+							{/each}
+						</div>
+						<div class="btnBox">
+							<a href={r.homepage} target="_blank" rel="noreferrer" class="btn"> View Live </a>
+							<a href={r.html_url} target="_blank" rel="noreferrer" class="btn btn-gh"> Github </a>
+						</div>
+					</div>
+				{/if}
+			{/each}
+		{:catch error}<p>{error.message}</p>
+		{/await}
+	</div>
+</section>
+
+<style lang="scss">
+	.projectCont {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-evenly;
+		gap: 2rem;
+		width: 100%;
+	}
+	.projectCard {
+		background: #111111 var(--bgImg) center / cover no-repeat;
+		width: 22.5rem;
+		height: 202.5px;
+		gap: 10px;
+		position: relative;
+		overflow: hidden;
+		z-index: 0;
+		transition: all 333ms;
+
+		> * {
+			transform: translateY(140px);
+			transition: all 333ms;
+		}
+		> *:not(h3) {
+			opacity: 0;
+		}
+
+		&::before {
+			content: '';
+			position: absolute;
+			z-index: -10;
+			background: linear-gradient(transparent, rgb(0 0 0 / 50%));
+			height: 3rem;
+			width: 100%;
+			bottom: 0;
+			left: 0;
+		}
+
+		&::after {
+			content: '';
+			position: absolute;
+			z-index: -10;
+			inset: 0;
+			transition: all 100ms;
+		}
+		&:hover {
+			transform: translateY(-4px);
+
+			&::after {
+				backdrop-filter: blur(8px);
+				background: rgb(17 17 17 / 33%);
+			}
+
+			> * {
+				transform: none;
+				opacity: 1;
+			}
+		}
+	}
+
+	h3 {
+		font-size: 1.25rem;
+		text-shadow: 0 0 8px black;
+	}
+	.topics {
+		color: var(--clr2);
+		display: flex;
+		gap: 1rem;
+		margin-top: auto;
+		text-transform: capitalize;
+	}
+	a {
+		display: flex;
+		align-items: center;
+		justify-content: space-evenly;
+	}
+</style>
